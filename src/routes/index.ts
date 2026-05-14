d6dd3a4739381e0590ad44263f421971fef5f5b9
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-14 13:52:54 +0200

    feat: Add root route to Fastify server that responds with a Hello World message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,4 +3,8 @@ import websiteToBlobImg from './website-to-blob-img.js';
 
 export default async function initRoutes(fastify: FastifyInstance) {
   await fastify.register(websiteToBlobImg);
+
+  fastify.get('/', async (_, reply) => {
+    return reply.status(200).send({ message: 'Hello World' });
+  });
 }
